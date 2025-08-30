a404572b6813dbabaeb989da678616c05350d7eb
Allie <allie.g.saoud@gmail.com>
2025-08-30 22:53:51 +0300

    removed unwanted imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import ScrollingContainer from "./components/ScrollingContainer";
